5ac4c0b28d4db2c9a73bc2fd1709fbb3a0df8cec
kaique <kaiquesilva@codeby.com.br>
2021-03-25 18:42:39 -0300

    first step, footer!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react';
+
+import CustomButton from '../src/components/custom-button/custom-button.component';
+import FormInput from '../src/components/form-input/form-input.component';
+
 function App() {
+
+  const [displayName,setdisplayName] = useState('');
+  const [email,setEmail] = useState('');
+  const [Text,setText] = useState('');
+  
+  
+  const handleSubmit = (event) =>{
+    
+    event.preventDefault();
+    console.log(displayName,email,Text);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <div className="body">
+        <form className='contact-me' onSubmit={handleSubmit}>
+          <h2>Manda uma mensagem :D</h2>
+            <FormInput
+            type='text'
+            name='displayName'
+            value={displayName}
+            onChange={(e) => setdisplayName(e)}
+            label='Nome'
+            required
+            />
+            <FormInput
+            type='text'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e)}
+            label='Email'
+            required
+            />
+            <FormInput
+            type='text'
+            name='Text'
+            value={Text}
+            onChange={(e)=> setText(e)}
+            label='Texto'
+            required
+            />
+            <CustomButton type='submit'>Send Message</CustomButton>
+          </form>
+      </div>
+      <div className="footer">
+        <h3>
+          Tel: (14) 99779-2464
+        </h3>
+        <h3>
+          Email: kaiqmo@gmail.com
+        </h3>
+        <h3>
+          Github: /kaiqmo
+        </h3>
+      </div>
     </div>
   );
 }
